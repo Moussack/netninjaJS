44e9c812768998b95de475a6580ba1fc2657d0cc
Moussack <reywahyono@gmail.com>
2021-10-07 19:53:27 +0700

    add message done

--- nimbleChat/scripts/app.js
+++ nimbleChat/scripts/app.js
@@ -1,9 +1,11 @@
-// ui.js Responsible for bringing(Gluing) chat.js and ui.js together and running the application
+// ui.js Responsible for bringing(Gluing) chat.js and ui.js..
+// together and running the application
 
 // DOM query
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
 
-// clasees instances
+// class instances
 const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom('general', 'rey');
 
@@ -11,4 +13,21 @@ const chatroom = new Chatroom('general', 'rey');
 chatroom.getChats((data) => {
    chatUI.render(data);
    console.log(data);
+});
+
+// add new chat
+newChatForm.addEventListener('submit', (e) => {
+   e.preventDefault();
+
+   const message = newChatForm.message.value.trim();
+
+   // *** add the message
+   chatroom
+      .addChat(message)
+      .then(() => {
+         newChatForm.reset();
+      })
+      .catch((err) => {
+         console.log(err);
+      });
 });
