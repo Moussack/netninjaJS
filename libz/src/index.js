5ed800384a1db39a31a853032a4e202c6da1f583
Moussack <reywahyono@gmail.com>
2021-10-22 14:03:15 +0700

    dropdown done

--- libz/src/index.js
+++ libz/src/index.js
@@ -1,7 +1,14 @@
 import './ninja-ui/styles/style.css';
-import './ninja-ui/styles/tooltip.css';
 import Tooltip from './ninja-ui/tooltips';
+import Dropdown from './ninja-ui/dropdown';
 
 // create tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
 tooltip.init();
+
+// create dropdowns
+const dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach((dropdown) => {
+   const instance = new Dropdown(dropdown);
+   instance.init();
+});
