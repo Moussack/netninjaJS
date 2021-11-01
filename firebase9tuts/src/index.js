ea76c4d997785514028f062ea93d8a27b0366a6c
Moussack <reywahyono@gmail.com>
2021-11-01 14:11:45 +0700

    init firebase done

--- firebase9tuts/src/index.js
+++ firebase9tuts/src/index.js
@@ -1,4 +1,6 @@
 import './styles.css';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
 
 const firebaseConfig = {
    apiKey: 'AIzaSyARAnY8tkeduEZX5CKClI2EzcYNz46iITk',
@@ -8,3 +10,22 @@ const firebaseConfig = {
    messagingSenderId: '67933165596',
    appId: '1:67933165596:web:33e0e991721da3883f67d9',
 };
+
+// init firebass app
+const app = initializeApp(firebaseConfig);
+// init firestore DB
+const db = getFirestore(app);
+// get collection reference
+const collRef = collection(db, 'books');
+// get collection data
+getDocs(collRef)
+   .then((snapshot) => {
+      let books = [];
+      snapshot.docs.forEach((doc) => {
+         books.push({ ...doc.data(), id: doc.id });
+      });
+      console.log(books);
+   })
+   .catch((err) => {
+      console.log(err.message);
+   });
