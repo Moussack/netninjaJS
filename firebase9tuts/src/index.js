be7372a84d07841ee11f8c381585796904c525b5
Moussack <reywahyono@gmail.com>
2021-11-01 12:25:04 +0700

    first commit

---
+++ firebase9tuts/src/index.js
@@ -0,0 +1,10 @@
+import './styles.css';
+
+const firebaseConfig = {
+   apiKey: 'AIzaSyARAnY8tkeduEZX5CKClI2EzcYNz46iITk',
+   authDomain: 'fir-9-dojo-nimble.firebaseapp.com',
+   projectId: 'fir-9-dojo-nimble',
+   storageBucket: 'fir-9-dojo-nimble.appspot.com',
+   messagingSenderId: '67933165596',
+   appId: '1:67933165596:web:33e0e991721da3883f67d9',
+};
